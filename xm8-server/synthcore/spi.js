5b79a1dbf0d4a5c72b0d55dd91714f0aad173994
Joakim Tysseng <joakim@tysseng.no>
2016-01-03 19:35:23 +0000

    Working input interrupt on pin 3. NB: pin 26 cannot be used as it is reserved for SPI

--- xm8-server/synthcore/spi.js
+++ xm8-server/synthcore/spi.js
@@ -10,16 +10,21 @@ var readCallback;
 
 // watch changes on pin that indicates that the SPI slave has data it wants transferred 
 // to the master.
-function initInterrupt(){
+// NB: pin numbers are physical pin numbers, not gpioX-pin number. (e.g. GPIO7 = pin 26)
+function initGPIO(){
+  
   gpio.on('change', function(channel, value) {
     if(value > 0){
       console.log("Interrupt triggered on SPI slave transmit pin");
-      read();    
+//      read();
     } else {
       console.log("Interrupt reset on SPI slave transmit pin");
     }
   });
-  gpio.setup(config.spi.interruptPin, gpio.DIR_IN, gpio.EDGE_BOTH);
+  gpio.setup(config.spi.interruptPin, gpio.DIR_IN, gpio.EDGE_BOTH, function(err){
+    if(err) throw err;
+    console.log("Setup spi slave interrupt on physical pin " + config.spi.interruptPin);
+  });
 }
 
 function initSPI(){
@@ -34,7 +39,7 @@ function initSPI(){
     config.spi.device, 
     spiConfig, 
     function(s){
-        s.open();
+      s.open();
     }
   );
 }
@@ -86,8 +91,18 @@ function write(buffer){
   });
 }
 
+function onExit(){
+  console.log("Cleaning up SPI module");
+  gpio.destroy(function(){
+    console.log("Closed all GPIO pins");
+  });
+  process.exit(1);
+}
+
 initSPI();
-initInterrupt();
+initGPIO();
+
+process.on('SIGINT', onExit);
 
 module.exports.write = write;
 module.exports.setReadCallback = setReadCallback;
