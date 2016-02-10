10aa90ecdd129ca82359a912d52ced08af1accb6
unknown <Joakim@Gigasort.(none)>
2016-02-10 15:26:15 +0100

    Working XM8 DAC board

--- EasyPIC/DacTest/DacTest.c
+++ EasyPIC/DacTest/DacTest.c
@@ -1,8 +1,20 @@
 #define TX_INTERRUPT_TRIS TRISB1_bit
+
+// PCB:
+
+#define A0 LATD2_bit
+#define A1 LATD3_bit
+#define _WR LATD4_bit
+#define LDAC LATD5_bit
+#define _RS LATD0_bit
+
+//Prototypekort
+/*
 #define A0 LATD2_bit
 #define A1 LATD3_bit
 #define _WR LATD0_bit
 #define LDAC LATD1_bit
+  */
 
 #define DATA_LO LATA // porta/l - D0-D7
 #define DATA_HI LATB // portb/l - D8-D15
@@ -20,6 +32,16 @@ void main() {
   // disable JTAG to get control of all pins on PORTA/L
   JTAGEN_bit = 0;
   
+  //Set ports as output
+  TRISB = 0;
+  LATB = 0;
+
+  TRISD = 0;
+  LATD = 0;
+
+  TRISA = 0;
+  LATA = 0;
+  
   // select DAC A
   A0 = 0;
   A1 = 0;
@@ -27,21 +49,11 @@ void main() {
   // set
   _WR = 1;
   LDAC = 0;
+  _RS = 1;
   
   delay_ms(1000);
-  
-  //Set ports as output
-  TRISB = 0;
-  LATB = 0;
 
-  TRISD = 0;
-  LATD = 0;
-  
-  TRISA = 0;
-  LATA = 0;
-  
   while(1){
-
     if(DATA_LO != 0){
       DATA_LO = 0;
       DATA_HI = 0;
@@ -49,18 +61,15 @@ void main() {
       DATA_LO = 0xFF;
       DATA_HI = 0xFF;
     }
-    delay_ms(500);
 
     // load input register
     _WR=0;
     LDAC=0;
-    delay_ms(500);
-    
+
     // load DAC register
     _WR=1;
     LDAC=1;
-    delay_ms(500);
-    
+
     // reset state
     LDAC=0;
 
