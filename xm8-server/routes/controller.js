05b3f02380aac25305a8b4d41c3df72a815aaf28
Joakim Tysseng <joakim@tysseng.no>
2016-02-11 16:27:48 +0100

    Trying to fix spi return to gui

--- xm8-server/routes/controller.js
+++ xm8-server/routes/controller.js
@@ -7,34 +7,36 @@ module.exports = function(app, ws){
 
   var root = "/controller";
 
-	app.ws("/controller", function(ws, req) {
-	  console.log("Something connected to controller");
+  app.ws("/controller", function(ws, req) {
+    console.log("Something connected to controller");
 
-	  ws.on('message', function(msg) {
-	    controller.publishControllerChange(msg);
-	  });
-	});
+    ws.on('message', function(msg) {
+      controller.publishControllerChange(msg);
+    });
+  });
 
-	// register web socket return function
-	var controllerWss = ws.getWss(root);
-	controller.onControllerChange = tools.sendToAllClients.bind(null, controllerWss);
+  // register web socket return function
+  var controllerWss = ws.getWss(root);
+  controller.onControllerChange = function(data){
+    tools.sendToAllClients.bind(controllerWss, data); 
+  }
 
-	/*
-	app.ws('/echo', function(ws, req) {
-	  console.log("Something connected to echo resource");
-	  ws.on('message', function(msg) {
-	    console.log("message received");
-	    publishControllerChange(msg);
+  /*
+  app.ws('/echo', function(ws, req) {
+    console.log("Something connected to echo resource");
+    ws.on('message', function(msg) {
+      console.log("message received");
+      publishControllerChange(msg);
 
-	    var aWebSocketService = ws.getWss('/echo');
-	    aWebSocketService.clients.forEach(function (client) {
-	      try{
-	        console.log("Resending " + msg);
-	        client.send(msg);
-	      } catch (ex){
-	        console.log("Client not available");
-	      }
-	    });    
-	  });
-	});*/
+      var aWebSocketService = ws.getWss('/echo');
+      aWebSocketService.clients.forEach(function (client) {
+        try{
+          console.log("Resending " + msg);
+          client.send(msg);
+        } catch (ex){
+          console.log("Client not available");
+        }
+      });    
+    });
+  });*/
 }
