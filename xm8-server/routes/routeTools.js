ab4e1dbfcfb9be3f51dbff353447438bc8d93210
Joakim Tysseng <joakim@tysseng.no>
2016-02-12 16:24:24 +0100

    inverted if to see if it works with client detection

--- xm8-server/routes/routeTools.js
+++ xm8-server/routes/routeTools.js
@@ -4,11 +4,11 @@ function sendToAllClients(wss, message){
   wss.clients.forEach(function (client) {
     console.log(client);
     try{
-      if(client !== message.source){
-        console.log("Sending message " + message + " to gui " + client);    
-        client.send(message);
-      } else {
+      if(client === message.source){
         console.log("Not sending message back to its origin");
+      } else {
+        console.log("Sending message " + message + " to gui " + client);    
+        client.send(message);        
       }
     } catch (ex){
       console.log("Client went missing before server could send message");
