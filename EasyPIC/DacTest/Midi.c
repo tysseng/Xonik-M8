4004022a5f6227f516659143258fce7096e82657
unknown <Joakim@Gigasort.(none)>
2016-04-15 22:06:27 +0200

    Added controller-to-matrix mapping for 7 and 14 bit controllers

--- EasyPIC/DacTest/Midi.c
+++ EasyPIC/DacTest/Midi.c
@@ -4,6 +4,16 @@
 #include "Types.h"
 #include "Matrix.h"
 
+// TODO: Configure controllers via SPI.
+
+// maps midi controllers to input positions in the matrix
+void MIDI_controllerToInputMap[127];
+
+// configures controller resolution, if set to 1 CC will only be written when
+// fine-res arrives.
+char MIDI_controllerHiRes[32];
+int lastLowResValue[32];
+
 void MIDI_HOOK_treatTwoByteMessage(char channel, char status, char param1){
 }
 
@@ -14,6 +24,19 @@ void MIDI_HOOK_treatThreeByteMessage(char channel, char status, char param1, cha
       break;
     case SM_NOTE_OFF:
       MX_noteOff();
+      break;
+    case SM_CC:
+      if(param1 < 32 && MIDI_controllerHiRes[param1]){
+        // 14 bit CCs
+        if(param1 >= 0x20 && param1 <= 0x3F){
+          MX_nodeResults[MIDI_controllerToInputMap[param1]] = lastLowResValue[param1] << 7 | param2;
+        } else {
+          lastLowResValue[param1] = param2;
+        }
+      } else {
+        // 7 bit CCs
+        MX_nodeResults[MIDI_controllerToInputMap[param1]] = param2 << 8;
+      }
       break;
   }
 }
