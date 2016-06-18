50773e9026376942451af346ca8bb64e7635ec21
Joakim Tysseng <joakim@tysseng.no>
2016-06-18 20:26:03 +0200

    Added dropshadow and glow to icons

--- xm8-server/frontend/components/matrix/MenuItem.js
+++ xm8-server/frontend/components/matrix/MenuItem.js
@@ -10,6 +10,26 @@ const MenuItem = ({label, icon, onClick, selected, target = "Capa_1"}) => {
   return (
     <span className="menuicon">
       <svg className={svgClassName} onClick={onClick}>
+        <filter id="drop-shadow" width="1.5" height="1.5" x="-.25" y="-.25">
+          <feGaussianBlur in="SourceAlpha" stdDeviation="2.5" result="blur"/>
+          <feColorMatrix result="bluralpha" type="matrix" values=
+                  "1 0 0 0   0
+                   0 1 0 0   0
+                   0 0 1 0   0
+                   0 0 0 0.9 0 "/>
+          <feOffset in="bluralpha" dx="1" dy="1" result="offsetBlur"/>
+          <feMerge>
+            <feMergeNode in="offsetBlur"/>
+            <feMergeNode in="SourceGraphic"/>
+          </feMerge>
+        </filter>   
+        <filter id="glow">
+          <feGaussianBlur stdDeviation="2.5" result="coloredBlur"/>
+          <feMerge>
+            <feMergeNode in="coloredBlur"/>
+            <feMergeNode in="SourceGraphic"/>
+          </feMerge>
+        </filter>    
         <use xlinkHref={"img/icons/" + icon + "#" + target}></use>
       </svg>
       <div className={labelClassName}>{label}</div>
