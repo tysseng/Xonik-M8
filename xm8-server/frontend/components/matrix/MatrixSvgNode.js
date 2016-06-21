4fbca38dcd4f664e4ea812e5546e5620e19498a0
Joakim Tysseng <joakim@tysseng.no>
2016-06-21 18:55:47 +0200

    Moved click to svg group to enable clicking on description/icon of node

--- xm8-server/frontend/components/matrix/MatrixSvgNode.js
+++ xm8-server/frontend/components/matrix/MatrixSvgNode.js
@@ -16,7 +16,8 @@ class MatrixSvgNode extends Component {
     this.svgNode.on('mousedown', () => {
         // set starting point for moving node
         let [x, y] = d3.mouse(document.getElementById(this.props.drawingAreaId));
-        let selectedNode = this.svgNode[0][0];    
+
+        let selectedNode = this.svgNode.select('.nodebox')[0][0];    
 
         // TODO: This is state, but not state that will survive a refresh gracefully...
         let offsetX = x - selectedNode.getAttribute('x');
@@ -52,8 +53,8 @@ class MatrixSvgNode extends Component {
     let className = this.getClassName('nodebox', node.id, this.props.selected, node.valid);
 
     return (
-      <g className='node' key={'nodegroup' + node.id}>
-        <rect ref='svgNode' data-node-id={node.id} key={'node' + node.id} x={node.vis.x} y={node.vis.y} rx="10" ry="10" width="40" height="40" className={className}/>
+      <g className='node' key={'nodegroup' + node.id} ref='svgNode'>
+        <rect data-node-id={node.id} key={'node' + node.id} x={node.vis.x} y={node.vis.y} rx="10" ry="10" width="40" height="40" className={className}/>
         <NodeIcon node={node} x={node.vis.x + 20 } y={node.vis.y + 24}/> 
         <text key={'label' + node.id} textAnchor="middle" x={node.vis.x + 20 } y={node.vis.y + 52} className='label'>{node.name}</text>
       </g>
