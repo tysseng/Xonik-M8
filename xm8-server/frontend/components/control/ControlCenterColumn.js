16d15006ff05d598d90f46be54c2e1382428894c
Joakim Tysseng <joakim@tysseng.no>
2016-06-28 22:23:42 +0200

    Tweaked fader looks

--- xm8-server/frontend/components/control/ControlCenterColumn.js
+++ xm8-server/frontend/components/control/ControlCenterColumn.js
@@ -6,12 +6,33 @@ const ControlCenterColumn = () => {
   return ( 
     <div>
       <div>Controls</div>
-      <div className="faderPane">
-        <Fader value={50}/>
-        <Fader value={50}/>
-        <Fader value={50}/>
-        <Fader value={50}/>
-        <Fader value={50}/>
+      <div className="faderRow">
+        <div className="faderGroup">
+          <Fader value={0} label='Attack'/>
+          <Fader value={10} label='Decay'/>
+          <Fader value={32} label='Sustain'/>        
+          <Fader value={20} label='Release'/>
+        </div>
+        <div className="faderGroup">
+          <Fader value={0} label='Attack'/>
+          <Fader value={10} label='Decay'/>
+          <Fader value={32} label='Sustain'/>        
+          <Fader value={20} label='Release'/>
+        </div>
+      </div>
+      <div className="faderRow">  
+        <div className="faderGroup">
+          <Fader value={0} label='Attack'/>
+          <Fader value={10} label='Decay'/>
+          <Fader value={32} label='Sustain'/>        
+          <Fader value={20} label='Release'/>
+        </div>
+        <div className="faderGroup">
+          <Fader value={0} label='Attack'/>
+          <Fader value={10} label='Decay'/>
+          <Fader value={32} label='Sustain'/>        
+          <Fader value={20} label='Release'/>
+        </div>
       </div>
     </div>
   )
--- xm8-server/frontend/components/control/Fader.js
+++ xm8-server/frontend/components/control/Fader.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 // http://fribly.com/2015/03/21/pretty-range-input-equalizer-style/
-const Fader = ({value}) => {
+const Fader = ({label, value, orientation = 'vertical', min = 0, max = 128, step = 1}) => {
+
   return (
-    <div className="range horizontal">
-      <input type='range' min='0' max='32' step='1' value='20'/>
+    <div className="range">
+      <div className={orientation}>
+        <input type='range' min={min} max={max} step={step} value={value}/>
+      </div>
+      <label>{label}</label>
     </div>
   )
 
