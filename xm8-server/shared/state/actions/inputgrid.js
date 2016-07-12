5136c44b98be63e44dde39fc567ca8185a4b4b60
Joakim Tysseng <joakim@tysseng.no>
2016-07-12 23:26:38 +0200

    Added disabled style to button

--- xm8-server/shared/state/actions/inputgrid.js
+++ xm8-server/shared/state/actions/inputgrid.js
@@ -1,7 +1,14 @@
 export const inputgridActionTypes = {
   SELECT_ELEMENT: 'SELECT_ELEMENT',
   MOVE_ELEMENT: 'MOVE_ELEMENT',
-  DESELECT_ELEMENT: 'DESELECT_ELEMENT'
+  DESELECT_ELEMENT: 'DESELECT_ELEMENT',
+  NEW_GROUP: 'NEW_GROUP',
+  LOAD_GROUP: 'LOAD_GROUP',
+  ADD_ELEMENT: 'ADD_ELEMENT',
+  DELETE_ELEMENT: 'DELETE_ELEMENT',
+  OPEN_NEW_ELEMENT_DIALOG: 'OPEN_NEW_ELEMENT_DIALOG',
+  CLOSE_NEW_ELEMENT_DIALOG: 'CLOSE_NEW_ELEMENT_DIALOG',
+  SELECT_ID_IN_NEW_ELEMENT_DIALOG: 'SELECT_ID_IN_NEW_ELEMENT_DIALOG'
 }
 
 let types = inputgridActionTypes;
@@ -14,7 +21,7 @@ export const selectElement = (id, mouseX, mouseY, offsetXem, offsetYem) => {
     offsetXem, 
     offsetYem,    
     id,
-    target: 'SERVER'
+    target: 'GUI'
   };  
 }
 
@@ -31,6 +38,67 @@ export const moveElement = (id, offsetXem, offsetYem) => {
 export const deselectElement = () => {
   return {
     type: types.DESELECT_ELEMENT,
+    target: 'GUI'
+  }
+}
+
+export const newGroup = (groupId) => {
+  return {
+    type: inputgridActionTypes.NEW_GROUP,
+    groupId,
     target: 'SERVER'
   }
+}
+
+export const loadGroup = (groupId) => {
+  return {
+    type: inputgridActionTypes.LOAD_GROUP,
+    groupId,
+    target: 'SERVER'
+  }
+}
+
+export const addElement = (id, groupId, elementId, elementType, offsetXem, offsetYem) => {
+  return {
+    type: inputgridActionTypes.ADD_ELEMENT,
+    id, 
+    groupId, 
+    elementId, 
+    elementType, 
+    offsetXem, 
+    offsetYem,
+    target: 'BOTH'
+  }
+}
+
+export const deleteElement = (id, groupId) => {
+  return {
+    type: inputgridActionTypes.DELETE_ELEMENT,
+    id, 
+    groupId,
+    target: 'SERVER'
+  }
+}
+
+export const openNewElementDialog = () => {
+  return {
+    type: inputgridActionTypes.OPEN_NEW_ELEMENT_DIALOG,
+    target: 'GUI'
+  }
+}
+
+export const closeNewElementDialog = () => {
+  return {
+    type: inputgridActionTypes.CLOSE_NEW_ELEMENT_DIALOG,
+    target: 'GUI'
+  } 
+}
+
+export const selectInputInNewElementDialog = (elementType, elementId) => {
+  return {
+    type: inputgridActionTypes.SELECT_ID_IN_NEW_ELEMENT_DIALOG,
+    elementId,
+    elementType,
+    target: 'GUI'
+  }   
 }
